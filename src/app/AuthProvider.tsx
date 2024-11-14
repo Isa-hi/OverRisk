a522ecca46e6ab62e3631c2b076b6d1018177547
Héctor Isahi Ochoa <lacasademiamigochanel@gmail.com>
2024-11-14 10:02:25 -0600

    Refactor AuthProvider component and add user logging in Navbar

--- src/app/AuthProvider.tsx
+++ src/app/AuthProvider.tsx
@@ -1,6 +1,5 @@
 "use client";
 import {KindeProvider} from "@kinde-oss/kinde-auth-nextjs";
-import React from "react";
 
 export const AuthProvider = ({children} : {children: React.ReactNode}) => {
   return <KindeProvider>{children}</KindeProvider>;
