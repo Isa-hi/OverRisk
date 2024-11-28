110f1ce5d068562623236d1fc93b13c6c597ea9b
Héctor Isahi Ochoa <lacasademiamigochanel@gmail.com>
2024-11-28 10:06:07 -0600

    Update layout height in dashboard and enhance button component with loading state

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -37,11 +37,13 @@ const buttonVariants = cva(
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  asChild?: boolean
+  asChild?: boolean,
+  isLoading?: boolean,
+  loadingText?: string,
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, isLoading, loadingText, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
     return (
       <Comp
